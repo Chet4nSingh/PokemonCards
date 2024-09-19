b21d0b86476ebfa7747a0587901105577bd6a984
Chetan Singh <chet4nsingh@gmail.com>
2024-09-19 12:47:15 +0530

    Add more sprites and loading spinner

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import Spinner from "./components/Spinner";
 
 function App() {
   const [cards, setCards] = useState([]);
@@ -8,34 +9,30 @@ function App() {
   useEffect(() => {
     async function fetchCards() {
       setLoading(true);
-      const res = await axios.get("https://pokeapi.co/api/v2/pokemon-form/1/");
-      const resData = res.data;
+      const promises = [];
+      for (let i = 1; i <= 100; i ++) {        
+        promises.push(axios.get(`https://pokeapi.co/api/v2/pokemon-form/${i}/`));
+      }      
+      const results = await Promise.all(promises);
+      const newCards = results.map(result => result.data);
 
-      // Add the card only if it doesn't exist in the state
-      setCards((prevState) => {
-        const exists = prevState.some((card) => card.id === resData.id);
-        if (!exists) {
-          return [...prevState, resData];
-        }
-        return prevState;
-      });
-
+      setCards(newCards);
       setLoading(false);
     }
 
     fetchCards();
-  }, []); // Empty dependency array ensures this runs once
+  }, []);
 
-  if (loading) return <p>Loading...</p>;
+  if (loading) return <Spinner />;
 
   return (
-    <main className="h-screen py-8 px-16 bg-black text-white">
-      <h1 className="text-6xl mb-8">Pokemon Cards</h1>
-      <ul>
+    <main className="min-h-screen py-8 px-16 bg-black text-white">
+      <h1 className="text-6xl pl-12 mb-16">Pokemon Cards</h1>
+      <ul className="flex flex-wrap gap-4 justify-center">
         {cards.map((card) => (
-          <li key={card.id} className="border p-8">
-            <img src={card.sprites.front_default} alt={card.name} />
-            <h2>{card.name.toUpperCase()}</h2>
+          <li key={card.id} className="w-[30%] border p-4 flex items-center gap-8">
+            <img src={card.sprites.front_default} alt={card.name} className="w-40 h-40" />
+            <h2 className="text-2xl">{card.name.toUpperCase()}</h2>
           </li>
         ))}
       </ul>
---
+++ src/components/Spinner.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+function Spinner() {
+  return (
+    <main className="h-screen w-full grid place-items-center">
+      <div className="boxes">
+        <div className="box">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="box">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="box">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="box">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default Spinner;
